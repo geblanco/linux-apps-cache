e984c2fa743c7c9b0066d70a45ab0a64c9026fa2
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-11-30 21:32:59 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # linux-apps-cache
 A .desktop files parser that caches all apps installed in your machine.
+
+Taken from [Cerebro app](https://github.com/KELiON/cerebro)
---
+++ index.js
@@ -0,0 +1,60 @@
+
+const glob = require('glob')
+const path = require('path')
+const fs = require('fs')
+const { flatten, uniq } = require('lodash')
+const { DIRECTORIES, PATTERNS, EXTENSIONS, formatPath,  } = require(path.join(__dirname || './', 'linux'))
+
+const REINDEX_TIME = 30 * 60 * 1000
+const CACHE_FILE = path.join(process.env.HOME, '.config', 'apps_cache.json')
+const cacheOptions = { encoding: 'utf-8' }
+
+const noop = (cb) => typeof cb === 'function' ? cb : function(){}
+
+const getAppsList = () => {
+  let patterns = DIRECTORIES.map(dir =>
+    path.join(dir, '**', `*.+(${EXTENSIONS.join('|')})`)
+  )
+  patterns = [
+    ...patterns,
+    ...(PATTERNS || [])
+  ]
+  const promises = patterns.map(pattern => (
+    new Promise((resolve, reject) => {
+      glob(pattern, {}, (err, files) => {
+        return err ? reject(err) : resolve(files)
+      })
+    })
+  ))
+  return Promise.all(promises).then(apps => (
+    uniq(flatten(apps)).map(formatPath).filter(app => !app.hidden)
+  ))
+}
+
+const scanApps = (callback) => getAppsList().then(apps => {
+  callback = noop(callback)
+  const json = JSON.stringify(apps, null, 2)
+  fs.writeFile(CACHE_FILE, json, cacheOptions, (err) => {
+    if (err) console.log('Error writing apps cache file')
+    callback(apps)
+  })
+})
+
+module.exports.init = (callback) => {
+  callback = noop(callback)
+  fs.exists(CACHE_FILE, (exists) => {
+    if (!exists){
+      scanApps(callback)
+    } else {
+      fs.readFile(CACHE_FILE, cacheOptions, (err, json) => {
+        if (!err) callback(JSON.parse(json))
+        else callback(err)
+      })
+    }
+  })
+
+  setInterval(scanApps, REINDEX_TIME)
+  DIRECTORIES.forEach(dir => fs.watch(dir, {}, scanApps))
+}
+
+module.exports.scanApps = scanApps
